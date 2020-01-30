a684c317855483755a1cdbd0f979947ea3a0d51d
NicholasHeilman <Nicholas.Heilman@gmail.com>
2020-01-30 10:31:27 -0500

    add SavedInstances to RadioButtons

--- app/src/main/java/com/example/android/fragmentexample/MainActivity.java
+++ app/src/main/java/com/example/android/fragmentexample/MainActivity.java
@@ -1,19 +1,25 @@
 package com.example.android.fragmentexample;
 
+import android.content.Intent;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import static com.example.android.fragmentexample.R.id.open_button;
 
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements SimpleFragment.OnFragmentInteractionListener {
+
 
     private Button mButton;
     private boolean isFragmentDisplayed = false;
+    private int mRadioButtonChoice = 2;
+    private static final String TAG = "MainActivity";
 
     // Saved instance state key.
     static final String STATE_FRAGMENT = "state_of_fragment";
@@ -38,9 +44,11 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 if (!isFragmentDisplayed) {
+                    Log.d(TAG, "onClick: Open");
                     displayFragment();
                 } else {
                     closeFragment();
+                    Log.d(TAG, "onClick: Close");
                 }
             }
         });
@@ -48,12 +56,12 @@ public class MainActivity extends AppCompatActivity {
 
     //Open fragment onClick
     public void displayFragment() {
-        // Instantiate the fragment.
-        SimpleFragment simpleFragment = SimpleFragment.newInstance();
+        // Initiate the fragment.
+        SimpleFragment simpleFragment = SimpleFragment.newInstance(mRadioButtonChoice);
         // Get the FragmentManager and start a transaction.
         FragmentManager fragmentManager = getSupportFragmentManager();
         FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-        // Add the SimpleFragment.
+       // Add the SimpleFragment.
         fragmentTransaction.add(R.id.fragment_container, simpleFragment).addToBackStack(null).commit();
         // Update the Button text.
         mButton.setText(R.string.close);
@@ -81,5 +89,12 @@ public class MainActivity extends AppCompatActivity {
     public void onSaveInstanceState(Bundle savedInstanceState) {
         savedInstanceState.putBoolean(STATE_FRAGMENT, isFragmentDisplayed);
         super.onSaveInstanceState(savedInstanceState);
+    }
+
+    @Override
+    public void onRadioButtonChoice(int choice) {
+        mRadioButtonChoice = choice;
+        Toast.makeText(this, "Choice is " + Integer.toString(choice), Toast.LENGTH_SHORT).show();
+
     }
 }
--- app/src/main/java/com/example/android/fragmentexample/SimpleFragment.java
+++ app/src/main/java/com/example/android/fragmentexample/SimpleFragment.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,33 +13,59 @@ import android.widget.RatingBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import static android.content.ContentValues.TAG;
 
 public class SimpleFragment extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
+
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final int YES = 0;
     private static final int NO = 1;
+    private static final int NONE = 2;
+
+    //default Radio Choice = NONE
+    public int mRadioButtonChoice = NONE;
+
+    // The "choice" key for the bundle.
+    private static final String CHOICE = "choice";
+    OnFragmentInteractionListener mListener;
 
     public SimpleFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Creates the view for the fragment.
-     *
-     * @param inflater           LayoutInflater to inflate any views in the fragment
-     * @param container          ViewGroup of parent view to attach fragment
-     * @param savedInstanceState Bundle for previous state
-     * @return rootView
-     */
+    interface OnFragmentInteractionListener {
+        void onRadioButtonChoice(int choice);
+    }
+
+
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public void onAttach(Context context) throws ClassCastException {
+        super.onAttach(context);
+        if (context instanceof OnFragmentInteractionListener) {
+            Log.d(TAG, "onAttach: instanceOf");
+            mListener = (OnFragmentInteractionListener) context;
+        } else {
+            Log.d(TAG, "onAttach: Exception");
+            throw new ClassCastException(context.toString()
+                    + getResources().getString(R.string.exception_message));
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
         // Inflate the layout for this fragment.
         final View rootView = inflater.inflate(R.layout.fragment_simple, container, false);
         final RadioGroup radioGroup = rootView.findViewById(R.id.radio_group);
         final RatingBar ratingBar = rootView.findViewById(R.id.ratingBar);
+
+        if (getArguments().containsKey( CHOICE )) {
+            mRadioButtonChoice = getArguments().getInt(CHOICE);
+            // Check the radio button choice.
+            if (mRadioButtonChoice != NONE) {
+                radioGroup.check(radioGroup.getChildAt(mRadioButtonChoice).getId());
+            }
+        }
 
         // Set the radioGroup onCheckedChanged listener.
         radioGroup.setOnCheckedChangeListener(
@@ -52,18 +79,25 @@ public class SimpleFragment extends Fragment {
                         switch (index) {
                             case YES: // User chose "Yes".
                                 textView.setText(R.string.yes_message);
+                                mRadioButtonChoice = YES;
+                                mListener.onRadioButtonChoice(YES);
                                 break;
                             case NO: // User chose "No".
                                 textView.setText(R.string.no_message);
+                                mRadioButtonChoice = NO;
+                                mListener.onRadioButtonChoice(NO);
                                 break;
                             default: // No choice made.
                                 // Do nothing.
+                                mRadioButtonChoice = NONE;
+                                mListener.onRadioButtonChoice(NONE);
                                 break;
                         }
                     }
                 });
-        // Set the rating bar onCheckedChanged listener.
-        ratingBar.setOnRatingBarChangeListener
+
+                // Set the rating bar onCheckedChanged listener.
+                ratingBar.setOnRatingBarChangeListener
                 (new RatingBar.OnRatingBarChangeListener() {
                     @Override
                     public void onRatingChanged(RatingBar ratingBar, float rating, boolean fromUser) {
@@ -73,12 +107,14 @@ public class SimpleFragment extends Fragment {
                                 Toast.LENGTH_SHORT).show();
                     }
                 });
+                return rootView;
+        }
 
-        // Return the View for the fragment's UI.
-        return rootView;
+        public static SimpleFragment newInstance(int choice) {
+            SimpleFragment fragment = new SimpleFragment();
+            Bundle arguments = new Bundle();
+            arguments.putInt(CHOICE, choice);
+            fragment.setArguments(arguments);
+            return fragment;
+        }
     }
-
-    public static SimpleFragment newInstance() {
-        return new SimpleFragment();
-    }
-}
